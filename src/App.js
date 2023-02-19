c3d58574dce2e40da9918cd60e4acbce10acc9d6
Trapa-Eureka <son199@hanmail.net>
2023-02-19 21:36:32 +0900

    add nav menu

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import './App.css';
 import bg_img from "./assets/bg-img.jpg";
 import Hero from './components/Hero';
 import Slider from './components/Slider';
+import Navbar from './components/Navbar';
+
+const navbarLinks = [
+  { url: "#", title: "Home" },
+  { url: "#one", title: "Trips" },
+  { url: "#", title: "Rewards" },
+];
 
 function App() {
   return (
     <div className="App">
+      <Navbar navbarLinks={navbarLinks} />
       <Hero imageSrc={bg_img} />
       <Slider imageSrc={bg_img} title={"Title 01"} subtitle={"subtitle 01"} />
       <Slider imageSrc={bg_img} title={"Title 02"} subtitle={"subtitle 02"} flipped={true} />
