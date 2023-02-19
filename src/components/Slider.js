f2df8b99baba2304f53bd4fd190a8f2fb1c0b026
Trapa-Eureka <son199@hanmail.net>
2023-02-19 20:15:57 +0900

    edit threshold rate

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,7 +5,7 @@ import { useInView } from 'react-intersection-observer';
 const Slider = ({ imageSrc, title, subtitle, flipped }) => {
 
   const { ref, inView } = useInView({
-    threshold: 0,
+    threshold: 0.4,
   });
 
   const renderContent = () => {
