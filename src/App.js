10bd85221d1f889fd39c84a30d551f86ac596368
Trapa-Eureka <son199@hanmail.net>
2023-02-19 14:11:11 +0900

    insert some img and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import bg_img from "./assets/bg-img.jpg";
+import Hero from './components/Hero';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Hero imageSrc={bg_img} />
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import "./Hero.css";
 
 const Hero = ({imageSrc}) => {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <img src={imageSrc} alt="testImage" className="hero__image" />
+      <h1 className="hero__title">made simple landing</h1>
+    </div>
+  );
 };
 
 export default Hero;
